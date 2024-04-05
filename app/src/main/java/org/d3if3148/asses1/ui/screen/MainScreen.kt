7905398f9bb7c16bb8d4cd907effae006a423de0
Fretty Simamora <purpieshomie@gmail.com>
2024-04-05 10:16:55 +0700

    Meminta input data dari pengguna

--- app/src/main/java/org/d3if3148/asses1/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3148/asses1/ui/screen/MainScreen.kt
@@ -1,17 +1,27 @@
 package org.d3if3148.asses1.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.d3if3148.asses1.R
@@ -39,12 +49,42 @@ fun MainScreen(){
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
+    var inc by remember { mutableStateOf("") }
+    var exps by remember { mutableStateOf("") }
+
     Column (
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-
+        Text(
+            text = stringResource(id = R.string.expense_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = inc,
+            onValueChange = {inc = it},
+            label = { Text(text = stringResource(R.string.income))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = exps,
+            onValueChange = {exps = it},
+            label = { Text(text = stringResource(R.string.expense))},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
