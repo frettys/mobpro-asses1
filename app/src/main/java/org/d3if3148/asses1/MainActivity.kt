16c3c6285789146ce78ae0aaf3714384825f4c72
Fretty Simamora <purpieshomie@gmail.com>
2024-04-05 09:41:08 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/org/d3if3148/asses1/MainActivity.kt
+++ app/src/main/java/org/d3if3148/asses1/MainActivity.kt
@@ -4,18 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3148.asses1.ui.screen.MainScreen
 import org.d3if3148.asses1.ui.theme.Asses1Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,39 +19,10 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen {
-                    }
+                ){
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen(content: @Composable (Modifier) -> Unit){
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        }
-    ) { padding ->
-        content(Modifier.padding(padding))
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ScreenPreview() {
-    Asses1Theme {
-
     }
 }
